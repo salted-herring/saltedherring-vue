e1718e392b29fcba8882ed4d616492ac3559de27
Simon Winter <simon@saltedherring.com>
2018-12-12 15:26:12 +1300

    removed vueexpress

--- src/routes.js
+++
@@ -1,7 +0,0 @@
-import HomeView from 'views/HomeView';
-import NotFound from 'components/core/NotFound';
-
-export default [
-  { path: '/', component: HomeView },
-  { path: '*', component: NotFound },
-];
