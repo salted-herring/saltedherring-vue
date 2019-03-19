6c33867a62e245abaccea6c34e085c0d59ebcebc
Simon Winter <simon@saltedherring.com>
2019-03-19 18:20:48 +1300

    minor css update

--- components/AboutNavigation.vue
+++ components/AboutNavigation.vue
@@ -44,6 +44,7 @@ export default {
     position: absolute
     top: rem(90)
     left: 0
+    z-index: 12
     width: 100%
     display: flex
     align-items: center
@@ -64,24 +65,52 @@ export default {
       line-height: 1em
       padding: rem(4) rem(4) rem(4) rem(8)
       background: $picasso
+      text-decoration: none
+      position: relative
+
+      &:before
+        position: absolute
+        bottom: 0
+        left: 8px
+        width: 0
+        background: $black
+        height: 3px
+        display: block
+        content: ''
+        margin-right: rem(14)
+        transition: width 0.25s
 
       +until($tablet)
         font-size: rem(14)
         background: none
 
+        &:before
+          height: 2px
+
       &:first-child
         padding-left: rem(4)
+
+        &:hover
+          &:before
+            width: calc(100% - 24px)
+
+        &:before
+          left: 4px
 
       &:last-child
         &:after
           display: none
+
+        &:hover
+          &:before
+            width: calc(100% - 14px)
 
       &:after
         padding-left: rem(10)
         content: '.'
 
       &:hover
-      &.nuxt-link-active
-        text-decoration: underline
+        &:before
+          width: calc(100% - 28px)
 
 </style>
