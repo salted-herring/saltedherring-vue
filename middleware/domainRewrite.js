1d6351222b32a1b1ace89ce2a828fc1c9330e282
Simon Winter <simon@saltedherring.com>
2019-05-14 14:11:38 +1200

    bugfix on domain rewrite

--- middleware/domainRewrite.js
+++ middleware/domainRewrite.js
@@ -2,10 +2,10 @@ export default function(req, res, next) {
   const host = req.headers.host
   const url = req.url
   const env = process.env.NODE_ENV
-  const canonicalDomain = process.env.BASE_URL
+  const canonicalDomain = process.env.CANONICAL_NAME
 
   if (env === 'production' && host !== canonicalDomain) {
-    res.writeHead(301, { Location: canonicalDomain + url })
+    res.writeHead(301, { Location: 'https://' + canonicalDomain + url })
     return res.end()
   }
 
